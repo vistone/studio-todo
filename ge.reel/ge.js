3b1f9b51391bb4b86d3492a17baa325e7c811ddd
stone <noreply>
2015-02-19 23:51:06 +0000

    Add component ge

---
+++ ge.reel/ge.js
@@ -0,0 +1,17 @@
+/**
+ * @module //ge.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Ge
+ * @extends Component
+ */
+exports.Ge = Component.specialize(/** @lends Ge# */ {
+    constructor: {
+        value: function Ge() {
+            this.super();
+        }
+    }
+});

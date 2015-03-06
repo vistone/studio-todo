e52a93ca2ac936c72633adc7dfde70df322d1c43
stone <noreply>
2015-03-06 08:59:22 +0000

    Add module ge

---
+++ ge.js
@@ -0,0 +1,16 @@
+/**
+ * @module //ge
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Ge
+ * @extends Montage
+ */
+exports.Ge = Montage.specialize(/** @lends Ge# */ {
+    constructor: {
+        value: function Ge() {
+            this.super();
+        }
+    }
+});

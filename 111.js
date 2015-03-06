82bd0dbc79977e0be4db26bae7af75367d375e7b
stone <noreply>
2015-03-06 08:59:56 +0000

    Add module 111

---
+++ 111.js
@@ -0,0 +1,16 @@
+/**
+ * @module //111
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class 111
+ * @extends Montage
+ */
+exports.111 = Montage.specialize(/** @lends 111# */ {
+    constructor: {
+        value: function 111() {
+            this.super();
+        }
+    }
+});
